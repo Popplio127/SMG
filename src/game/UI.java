cc79d96baa1104a4a630d0970b1f0219c463dc62
pietr <pietr@PCPIE2.Home>
2025-04-03 19:19:07 +0200

    m

--- src/game/UI.java
+++ src/game/UI.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface UI {
     public void showMessage(String msg);
+    public void setIsDadoTirato(boolean isDadoTirato);
     public void showBoard(String[][] board, List<Carta> manoCarte);
     public void makeMove();
 }
